8bc638635fa26b9e36a5a34177d7d89fe5b4144e
Maziar Ghorbani <maziar.ghorbani@cern.ch>
2020-02-18 19:30:14 +0100

    V2 tested with II=1

--- TrackFindingTMTT/interface/HLS/LRHLS_types.h
+++ TrackFindingTMTT/interface/HLS/LRHLS_types.h
@@ -31,20 +31,20 @@ enum {
 };
 
 // Fixed-point variables used in HW
-// typedef ap_uint<B1> uint1_t;
-// typedef ap_uint<B3> uint3_t;
-// typedef ap_uint<B4> uint4_t;
-// typedef ap_int<B13> int13_t;
-// typedef ap_int<B14> int14_t;
-// typedef ap_fixed<WB,IB> dtf_t;
+//typedef ap_uint<B1> uint1_t;
+//typedef ap_uint<B3> uint3_t;
+//typedef ap_uint<B4> uint4_t;
+//typedef ap_int<B13> int13_t;
+//typedef ap_int<B14> int14_t;
+//typedef ap_fixed<WB,IB> dtf_t;
 
 // Fixed-point variables used in SW
 typedef ap_uint<B1> uint1_t;
 typedef ap_uint<B3> uint3_t;
 typedef ap_uint<B4> uint4_t;
-typedef ap_fixed<B13,9> int13_t;
-typedef ap_fixed<B14,10> int14_t;
-typedef ap_fixed<WB,IB> dtf_t;
+typedef ap_fixed<WB,B13> int13_t;
+typedef ap_fixed<WB,B14> int14_t;
+typedef ap_fixed<WB+IB,IB+IB> dtf_t;
 
 // Fixed-point variables used in Tests
 //typedef float int13_t;
